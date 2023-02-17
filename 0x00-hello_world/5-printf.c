cd1bd9436ad60ec2690b3335d6e6b6e3dc2fba99
Bakare Alice <alisco20032001@gmail.com>
2023-02-17 04:22:00 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio h>
+#include <stdio.h>
 /**
- * main - print a c programm using printf
+ * main - print c programme using printf
  *
  * Return: 0
- */
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
