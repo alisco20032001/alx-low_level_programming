0234e3f56c04206bc1e3f9d814ec53369afcb47f
Bakare Alice <alisco20032001@gmail.com>
2023-02-18 15:15:00 -0800

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,2 @@
-#include <stdio.h>
-/**
- * main - A program that prints the size of various types on the computer types
- * Return: 0
- */
-int main(void)
-{
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+rm -r ".6-size.c.swp"
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
