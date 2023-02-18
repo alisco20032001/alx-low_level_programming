72b5e2ce88067bda2cb18f92c907db67fd1fa176
Bakare Alice <alisco20032001@gmail.com>
2023-02-18 13:58:05 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i1, i2, i3;
+
+	for (i1 = 0; i1 < 8; i1++)
+	{
+		for (i2 = i1 + 1; i2 < 9; i2++)
+		{
+			for (i3 = i2 + 1; i3 < 10; i3++)
+			{
+				putchar((i1 % 10) + '0');
+				putchar((i2 % 10) + '0');
+				putchar((i3 % 10) + '0');
+
+				if (i1 == 7 && i2 == 8 && i3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
