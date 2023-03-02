d0582c0405c39c041ba3d9b66dcc97b5079ab788
Bakare Alice <alisco20032001@gmail.com>
2023-03-02 10:35:11 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,8 +5,8 @@
  * @s1: first string to compare
  * @s2: second string to compare
  *
- * Return: < 0 if s1 < s2, 0 if they're equal,
- * more than 0 if s1 > s2
+ * Return: 0 if s1 is less than  s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
  */
 int _strcmp(char *s1, char *s2)
 {
