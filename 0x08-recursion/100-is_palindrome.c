35949e7e96aa651d8e443f309c178e40fbcb5548
Bakare Alice <alisco20032001@gmail.com>
2023-03-08 09:36:34 -0800

    3-factorial.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _palindrome.c - function that returns 1 if a string
+ * is a palindrome and 0 if not.
+ * @s: string
+ *
+ * Return: 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_palindrome(char *s);
+{
+	if (*s == 0 || *(s + 1) == 0)
+		return 1;
+	
+	_putchar = ('\n');
+	ispalindrome(str + 1);
+}
+
