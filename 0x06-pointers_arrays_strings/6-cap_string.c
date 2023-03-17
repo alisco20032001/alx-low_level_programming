518e66336e649f6df4d3115f6718188a540ea871
Bakare Alice <alisco20032001@gmail.com>
2023-03-17 07:52:52 -0700

    0-malloc_checked.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
  * cap_string - function that capitalizes everey word of a string
  * @s: string to modify
  *
- * Return: the resulting string
+ * Return: to  string
  */
 char *cap_string(char *s)
 {
@@ -15,19 +14,19 @@ char *cap_string(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
+	if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+	s[i] -= 32;
 
-		for (m = 0; m < 13; m++)
-		{
-			if (s[i] == spe[m])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] -= 32;
-				}
-			}
-		}
+	for (m = 0; m < 13; m++)
+	{
+	if (s[i] == spe[m])
+	{
+	if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+	{
+	s[i + 1] -= 32;
 	}
-	return (s)
+	}
+	}
+	}
+	return (s);
 }
