a5f28dfe6e78c7e4062a9aeb5b8e2c95dad4bad5
Bakare Alice <alisco20032001@gmail.com>
2023-06-09 10:41:33 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of
+ * arguments passed into it.
+ * @argc: number of argument
+ * @argv: array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
