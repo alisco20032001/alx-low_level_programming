7127d65068d9666dc41a16d728269cb3be529dc4
Bakare Alice <alisco20032001@gmail.com>
2023-06-09 10:46:32 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argc: number of argument
+ * @argv: array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int j;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (j = 0; *argv; j++, argv++)
+			;
+
+		printf("%d\n", j - 1);
+	}
+
+	return (0);
+}
