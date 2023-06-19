804b795cc91c10fa92d0ec076d44955954957b8e
Bakare Alice <alisco20032001@gmail.com>
2023-06-19 08:02:04 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+
+/**
+*struct dog - new type of data
+*@name: dog name
+*@age: dog age
+*@owner: name of owner
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+/**
+*dog_t - struct dog typedef
+*/
+
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
