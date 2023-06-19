2139a4b2ef8ca691c887435246c886a0d4898020
Bakare Alice <alisco20032001@gmail.com>
2023-06-19 10:17:30 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 
 /**
@@ -20,15 +21,16 @@ char *owner;
 
 
 /**
-*dog_t - struct dog typedef
+* struct_dog_t - struct dog typedef
 */
 
 typedef struct dog dog_t;
-
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 
 #endif
