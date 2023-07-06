48436882ecb43d85a2208066a4e747914ef19e0b
Bakare Alice <alisco20032001@gmail.com>
2023-07-06 05:56:54 -0700

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,38 +1,14 @@
-#include <stdio.h>
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-void __attribute__((constructor)) hare(void);
-
-/**
- * hare - Prints a string before the
- *        main function is executed.
- */
-void hare(void)
-{
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
-}
-
-hello hoberton
-
-extern printf
-
-section .text
-   global main
-
+	SECTION .text
+	extern printf
+	global main
 main:
-   push rbp
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-   mov rdi,fmt
-   mov rsi,msg
-   mov rax,0
-   call printf
-
-   pop rbp
-
-   mov rax,0
-   ret
-
-section .data
-   msg: db "Hello, Holberton", 0
-   fmt: db "%s", 10, 0
-
+	mov eax, 0
